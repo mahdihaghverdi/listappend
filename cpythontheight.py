e26f0f8cb51942d89c3a4e6c77d0ae2459123f0a
MahdiHaghverdi <mahdihaghverdliewpl@gmail.com>
2023-06-30 21:28:28 +0330

    rename 3.8cpython

---
+++ cpythontheight.py
@@ -0,0 +1,27 @@
+import csv
+from sys import getsizeof
+
+
+def list_size(size: int):
+    """Add `size` objects to the list"""
+    lst = []
+    info_list = [(len(lst), getsizeof(lst))]
+    for num in range(size):
+        lst.append(num)
+        info_list.append((len(lst), getsizeof(lst)))
+    return info_list
+
+
+information = list_size(126)
+
+with open('3.8cpystatall.csv', 'w', newline='') as all_f:
+    fieldnames = ['list_size', 'getsizeof']
+    writer = csv.DictWriter(all_f, fieldnames=fieldnames)
+
+    writer.writeheader()
+    writer.writerows(
+        [
+            {'list_size': info[0], 'getsizeof': info[1]}
+            for info in information
+        ]
+    )
